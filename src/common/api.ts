f89adcbfcf3fe880592fe9650727056aa323291a
up9t <up9t99@gmail.com>
2026-08-24 17:20:21 +0700

    Update

--- src/common/api.ts
+++ src/common/api.ts
@@ -21,6 +21,7 @@ export async function showOverlay() {
   if (overlay) {
     await overlay.show();
     await overlay.setFocus();
+    await overlay.setAlwaysOnTop(true);
   }
 }
 
--- src/settings.ts
+++ src/settings.ts
@@ -3,6 +3,6 @@ import { reactive } from "vue";
 export const settings = reactive({
   thresholdEyesClosed: 0.5,
   thresholdEyesOpened: 0.4,
-  blinkTimeout: 0.5,
+  blinkTimeout: 1.0,
   notifyBeforeSecond: 20,
 });
